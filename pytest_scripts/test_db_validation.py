b3d1334053ad77dc1198c8ea3e47e6a621fb028f
Aravinthvs <aravinthsubramani100@gmail.com>
2026-03-16 19:03:41 +0530

    Converted pytest_scripts from submodule to normal folder

---
+++ pytest_scripts/test_db_validation.py
@@ -0,0 +1,21 @@
+import oracledb
+
+def test_record_count():
+
+    conn = oracledb.connect(
+        user="hr",
+        password="hr",
+        dsn="localhost:1521/orclpdb"
+    )
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT COUNT(*) FROM table1_data")
+    source_count = cursor.fetchone()[0]
+
+    cursor.execute("SELECT COUNT(*) FROM table2_data")
+    target_count = cursor.fetchone()[0]
+
+    conn.close()
+
+    assert source_count == target_count
